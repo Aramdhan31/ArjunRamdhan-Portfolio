cd31c4fd675af470b07e9eee0cf0fb92f79eca52
Aramdhan31 <160909844+Aramdhan31@users.noreply.github.com>
2025-06-02 10:17:46 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import {
-  Github,f
+  Github,
   Linkedin,
   ArrowDown,
   ExternalLink,
